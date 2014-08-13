5678ded5d4bc8ca1fe9309e2bcea7629143f1da1
GoodDoctor05 <shanesrf@gmail.com>
2014-08-13 11:32:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HamlFoundation::Application.config.secret_key_base = 'b97e8b25236a1e8775de8a86763eb443a3ccda278d3119e0365ebf381a3a949cbbe849fedbf9ddf71dcb4f00496d5909005c5d02bcd7d4146b7705105efaa2ed'
